c81097dfc13ea19a671142afe3bc3d9cc09eccc2
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-31 16:42:04 -0500

    Demonstrate basics of passing objects, looping through them in pug file.

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // Using PUG as our template engine; notice that PUG uses indentation instead of HTML tags
 
 const express = require('express'),
+			mongoose = require('mongoose'),
 			path = require('path'),
 			app = express();
 const port = process.env.PORT || 3000;
+
+// connect mongoose to our mongoDB database
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
 
 // Load our view engine, PUG
 app.set('views', path.join(__dirname, 'views'));
@@ -13,9 +18,29 @@ app.set('view engine', 'pug');
 app.get('/', (req, res) => {
 	// since we're passing in a view, we use the render method to, well, render it.
 	// note that we can also pass values to our view
+	let articles = [
+		{
+			id: 1,
+			title: 'Article One',
+			author: 'Charles Reed',
+			body: 'This is article one. It is good.'
+		},
+		{
+			id: 2,
+			title: 'Article Two',
+			author: 'Blarles Sneed',
+			body: 'This is article two. It is also good.'
+		},
+		{
+			id: 1,
+			title: 'Article Three',
+			author: 'Cathy Bates',
+			body: 'This is article three. It is a little frightening.'
+		},
+	]
 	res.render('index', {
 		title: 'Articles',
-		body: 'The title was passed in from app.js!'
+		articles: articles
 	});
 })
 
