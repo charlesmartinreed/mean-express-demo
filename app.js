2d79551ae5dd673cc36b1ddb2ff6fd0432dd075a
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-31 16:19:34 -0500

    Create new view for adding article. Create generic layout to be extended by other views.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// This ROUTE is the HOME route
 app.get('/', (req, res) => {
 	// since we're passing in a view, we use the render method to, well, render it.
 	// note that we can also pass values to our view
@@ -16,6 +17,13 @@ app.get('/', (req, res) => {
 		title: 'Articles',
 		body: 'The title was passed in from app.js!'
 	});
+})
+
+// This route is used for ADDING AN ARTICLE
+app.get('/articles/add', (req, res) => {
+	res.render('add_article', {
+		title: 'Add new article'
+	})
 })
 
 app.listen(port, () => {
