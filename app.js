88c278a7fcc6ed046c0fe1ab45c17943424d4a8a
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-31 16:11:39 -0500

    Demonstrate basics of adding pug as view engine.

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+// Using PUG as our template engine; notice that PUG uses indentation instead of HTML tags
+
 const express = require('express'),
+			path = require('path'),
 			app = express();
 const port = process.env.PORT || 3000;
 
+// Load our view engine, PUG
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-	res.send('hey.')
+	// since we're passing in a view, we use the render method to, well, render it.
+	// note that we can also pass values to our view
+	res.render('index', {
+		title: 'Articles',
+		body: 'The title was passed in from app.js!'
+	});
 })
 
 app.listen(port, () => {
