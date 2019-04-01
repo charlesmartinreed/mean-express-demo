f5e83a58c5292fd0086f3878a3d3a7cc2aeb8fa4
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-01 15:18:53 -0500

    Add ability to login and logout via passport.

--- routes/users.js
+++ routes/users.js
@@ -78,5 +78,12 @@ router.post('/login', (req, res, next) => {
 	})(req, res, next);
 });
 
+// GET ROUTE to logout
+router.get('/logout', (req, res) => {
+	req.logout();
+	req.flash('success', 'You are now logged out');
+	res.redirect('/users/login');
+})
+
 // Don't forget to export!
 module.exports = router;
