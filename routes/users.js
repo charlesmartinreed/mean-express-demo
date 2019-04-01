66d7708a40d4a2be01a5d418bd300bfcb8fcdb80
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-01 13:47:41 -0500

    Create routes for creating new user, new user log in.

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,71 @@
 const express = require('express'),
 			router = express.Router();
 
+const bcryptjs = require('bcryptjs');
+
 let User = require('../models/user');
 
 // GET REQUEST to register url
 router.get('/register', (req, res) => {
 	res.render('register');
 });
+
+// POST REQUEST to REGISTER user
+router.post('/register', (req, res) => {
+	const name = req.body.name;
+	const email = req.body.email;
+	const username = req.body.username;
+	const password = req.body.password;
+	const password2 = req.body.password2;
+
+	req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+	let errors = req.validationErrors();
+
+	if(errors) {
+		res.render('register', {
+			errors
+		});
+	} else {
+		// submit a new user to the DB
+		let newUser = new User({
+			name,
+			email,
+			username,
+			password
+		});
+
+		// salt and hash the password
+		bcryptjs.genSalt(10, (err, salt) => {
+			bcryptjs.hash(newUser.password, salt, (err, hash) => {
+				if(err){
+					console.log(err);
+				}
+					// set password to the hashed version, save to DB
+					newUser.password = hash;
+					newUser.save((err) => {
+						if(err) {
+							console.log(error);
+							return
+						} else {
+							req.flash('success', 'You are now registered and can log in');
+							res.redirect('/users/login');
+						}
+					})
+			});
+		});
+	}
+});
+
+// GET ROUTE to login
+router.get('/login', (req, res) => {
+	res.render('login');
+})
 
 // Don't forget to export!
 module.exports = router;
